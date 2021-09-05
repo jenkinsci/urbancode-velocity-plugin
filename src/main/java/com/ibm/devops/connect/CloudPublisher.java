3f6fa79594dea8913c566549b7a1b4226ce79730
Lalit Kumar <lalit-ku@hcl.com>
2021-09-05 13:56:30 +0530

    Update CloudPublisher.java

--- src/main/java/com/ibm/devops/connect/CloudPublisher.java
+++ src/main/java/com/ibm/devops/connect/CloudPublisher.java
@@ -17,8 +17,6 @@ package com.ibm.devops.connect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.ibm.devops.connect.DevOpsGlobalConfiguration;
-
 import net.sf.json.JSONObject;
 import net.sf.json.JSONArray;
 
